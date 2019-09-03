d8618c2026fafebcb46b7757a77a28552d22be33
zzfancitizen <zzfancitizen@gmail.com>
2019-09-03 22:52:03 +0800

    fix proto bug in token service

--- src/Token/src/grpcLoader.js
+++ src/Token/src/grpcLoader.js
@@ -2,9 +2,6 @@ const path = require('path');
 const PROTO_PATH = path.resolve(__dirname, '../proto/token.proto');
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
-
-// TODO: error load proto
-
 const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {
--- src/Token/src/tokenService.js
+++ src/Token/src/tokenService.js
@@ -9,40 +9,51 @@ function main() {
     let server = new grpc.Server();
     server.addService(token.CRUD.service, {
         getToken: (call, callback) => {
+            console.log("get token called");
             let key = "";
-            call.key.value ? key = call.key.value : key = 'CreateAsset';
+            call.redisKey.keyValue ? key = call.redisKey.keyValue : key = 'CreateAsset';
             let token = crypto.randomBytes(32).toString('hex');
             let redisClient = redis.createClient();
             redisClient.on("error", (err) => {
-                callback(null, {token: null, error: err});
+                callback(null, {tokenValue: null, error: err, isSuccess: false});
             });
             // set key token pair to redis
             redisClient.set(key, token);
             redisClient.quit();
-            callback(null, {token: token, error: null});
+            callback(null, {tokenValue: token, error: null, isSuccess: true});
         },
         checkToken: (call, callback) => {
+            console.log("check token called");
             let redisClient = redis.createClient();
             redisClient.on("error", (err) => {
-                callback(null, {token: null, error: err});
+                callback(null, {tokenValue: null, error: err, isSuccess: false});
             });
-            redisClient.get(call.check_value.key, (err, reply) => {
+            redisClient.get(call.checkValue.keyValue, (err, reply) => {
                 if (err) callback(null, {return: false});
-                if (reply.toString() === call.check_value.token) {
-                    callback(null, {return: true})
-                } else {
-                    callback(null, {return: false});
+                if (reply.toString() === call.checkValue.tokenValue) {
+                    callback(null, {tokenValue: call.checkValue.tokenValue, error: null, isSuccess: true})
                 }
             });
             redisClient.quit();
         },
         deleteToken: (call, callback) => {
+            console.log("delete token called");
             let redisClient = redis.createClient();
             redisClient.on("error", (err) => {
-                callback(null, {token: null, error: err});
+                callback(null, {tokenValue: null, error: err, isSuccess: false});
             });
-            redisClient.del(call.key.value);
+            redisClient.del(call.redisKey.keyValue, (err, response) => {
+                if (response === 1) {
+                    callback(null, {tokenValue: call.redisKey.keyValue, error: null, isSuccess: true});
+                } else {
+                    callback(null, {tokenValue: null, error: err, isSuccess: false})
+                }
+            });
             redisClient.quit();
         }
-    })
+    });
+    server.bind('localhost:50052', grpc.ServerCredentials.createInsecure());
+    server.start();
 }
+
+main();
