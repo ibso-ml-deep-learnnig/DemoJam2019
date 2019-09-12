e436392222fed646babffba9d955371570ddd262
MatthewGeekTalk <matthew.he.geek@gmail.com>
2019-09-12 14:56:10 +0800

    add k8s yaml file

--- src/AssetService/src/front2asset-server.js
+++ src/AssetService/src/front2asset-server.js
@@ -50,7 +50,7 @@ function main() {
     });
     server.addService(health.Health.service, {
         Check: (call, callback) => {
-            callback(null, { status: 'SERVING' });
+            callback(null, { status: 'SERVING' })
         });
     server.bind(address, grpc.ServerCredentials.createInsecure());
     server.start();
