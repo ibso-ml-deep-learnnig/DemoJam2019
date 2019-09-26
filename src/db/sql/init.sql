d4e6b4d50d3b9bd188e0d6a93d4611163d8c8a59
i333463 <34474792+i333463@users.noreply.github.com>
2019-09-26 14:06:48 +0800

    new init.sql for asset

--- src/db/sql/init.sql
+++ src/db/sql/init.sql
@@ -12,6 +12,24 @@ CREATE TABLE log (
   error VARCHAR(50)
 );
 
+CREATE TABLE asset (
+  asset_id char(36) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  asset_class char(6) NOT NULL,
+  description text NOT NULL,
+  picture char(50) NOT NULL,
+  company_coe char(4) NOT NULL,
+  asset_number char(10) NOT NULL,
+  asset_subno char(4) NOT NULL,
+  cost_center char(10) NOT NULL,
+  acquisiton_date date NOT NULL,
+  amount double unsigned zerofill NOT NULL,
+  ul_year smallint(5) unsigned zerofill NOT NULL,
+  ul_period smallint(5) unsigned zerofill NOT NULL,
+  user_id char(10) NOT NULL,
+  create_date date NOT NULL,
+  create_time time NOT NULL
+);
+
 INSERT INTO user (user_id, user_name, password)
 VALUES
 ('i333463',	'Eric Wu',	'123456'),
